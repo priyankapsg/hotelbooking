ca5e818a0ed9a69286280d1bb6ecbf57cb89564d
maninstore <manisekar.s@nstore.in>
2023-10-29 21:52:37 +0530

    stripe changes

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -2,16 +2,30 @@ const express = require("express");
 const router = express.Router();
 const Booking = require('../models/booking');
 const Room = require('../models/room');
+const { v4: uuidv4 } = require('uuid');
+const stripe = require('stripe')('sk_test_51O6ZReSE0UrCVwmmaKx7HKNPxdTkK6NLowOcfG84jpVw9vRVz5QWp7HHPo0s985pwiL7uRufWabKTZ7BANazyq2P00B93647Oj');
+
 router.post("/bookroom",async(req,res)=>{
-    const {room,
-        userid,
-        fromdate,
-        todate,
-        totalamount,
-        totaldays} = req.body
+    const {room,userid,fromdate,todate,totalamount,totaldays,token} = req.body
 
+try {
+    
+const customer  = await stripe.customers.create({
+    email : token.email,
+    source: token.id
+})
 
-    try {
+const payment = await stripe.charges.create({
+    amount: totalamount * 100,
+    customer: customer.id,
+    currency: 'inr',
+    receipt_email: token.email
+    },
+    {
+     idempotencyKey : uuidv4()
+    })
+
+if(payment){
         const newbooking = new Booking({
             room: room.name,
             roomid: room._id,
@@ -27,10 +41,13 @@ router.post("/bookroom",async(req,res)=>{
         const roomtemp = await Room.findOne({_id:room._id});
         roomtemp.currentbookings.push({bookingid:booking._id, fromdate: fromdate, todate:todate,userid:userid,status:booking.status})
         await roomtemp.save();
-        res.send("Room booked succesfully")
-    } catch (error) {
-        return res.status(400).json({message:error});
-    }
+        res.send('Payment successful, your room booking is confirmed.')
+}
+
+} catch (error) {
+    return res.status(400).json([error])
+}
+
 });
 
 router.post("/getbookingsbyuserid",async(req,res)=>{
